43c4c2fd9065cb9eabecd310bd3374c1e6ec75ff
beefchief <c.lang.1993@gmail.com>
2024-09-18 23:20:33 -0500

    state, render complete

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const COHORT = "lang.2408";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+const COHORT = "colby.lang.2408";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events/`;
 
 //STATE
 const state = {
@@ -16,7 +16,7 @@ async function getEvents(){
     catch (error) {
         console.error(error);
     }
-}
+};
 
 async function addEvent(event){
     try{
@@ -32,32 +32,79 @@ async function addEvent(event){
     } catch(error) {
         console.error(Error);
     }
-}
+};
+
+async function rmEvent(id){
+    try {
+        const response = await fetch(API_URL + id, {
+            method: "DELETE",
+        });
+        if(!response.ok) {
+            const respObj = await response.json();
+            throw new Error(respObj.error.message);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 
 //RENDER
 
 function renderEvents() {
     const eventList = document.querySelector("#events");
+
+    if (!state.events.length) {
+        eventList.innerHTML = `
+        <li>No events near you</li>`;
+        return;
+    }
+
     const $events = state.events.map((event) => {
         const $li = document.createElement("li");
         $li.innerHTML = `
         <h2>${event.name}</h2>
-        <p>${event.date}</p>
-        <p>${event.location}</p>
+        <time datetime="${event.date}">${event.date.slice(0, 10)}</time>
+        <address>${event.location}</address>
         <p>${event.description}</p>
-        <button>Remove Event</button>
+        <button id = "kill">Remove Event</button>
         `;
+
+        const $remove = $li.querySelector("#kill");
+        $remove.addEventListener("click", async () => {
+            
+            await rmEvent(event.id);
+            await getEvents();
+            renderEvents();
+        });
+
         return $li;
     });
 eventList.replaceChildren(...$events);    
-}
+};
 
 
+//SCRIPT
 async function render() {
     await getEvents();
     renderEvents();
-}
+};
 
-//SCRIPT
 render();
+
+const $form = document.querySelector("form");
+$form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    
+    const date = new Date($form.date.value).toISOString();
+
+    const newEvent = {
+        name: $form.name.value,
+        date,
+        location: $form.location.value,
+        description: $form.description.value,
+    };
+    await addEvent(newEvent);
+    await getEvents();
+    renderEvents();
+});
